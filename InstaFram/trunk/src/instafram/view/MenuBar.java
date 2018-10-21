2f64f43943f1660fb2560741604e939f4a163241
rn14-2017 <rn14-2017@7147980d-cb72-d84e-875d-273fa6e1a3de>
2018-10-21 22:51:56 +0000

    Dodat toolBar i ikonice

--- InstaFram/trunk/src/instafram/view/MenuBar.java
+++ InstaFram/trunk/src/instafram/view/MenuBar.java
@@ -1,5 +1,7 @@
 package instafram.view;
 
+import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -11,34 +13,48 @@ public class MenuBar extends JMenuBar{
 
 		JMenu file = new JMenu("File");
 		JMenuItem mNew = new JMenuItem("mNew");
+		mNew.setIcon(new ImageIcon("Img/new.png"));
 		
 		JMenuItem mOpen = new JMenuItem("mOpen");
+		mOpen.setIcon(new ImageIcon("Img/open.jpg"));
 		JMenuItem mClose = new JMenuItem("mClose");
+		mClose.setIcon(new ImageIcon("Img/close.png"));
 		JMenuItem mSwitch = new JMenuItem("mSwitch");
+		mSwitch.setIcon(new ImageIcon("Img/switch.png"));
 		
 		JMenuItem mSave = new JMenuItem("mSave");
+		mSave.setIcon(new ImageIcon("Img/save.png"));
 		JMenuItem mSaveAs = new JMenuItem("mSave");
+		mSaveAs.setIcon(new ImageIcon("Img/saveAs.png"));
 		
 		JMenu mExport = new JMenu("mExport");
 		JMenuItem mExpToPdf = new JMenuItem("mExpToPdf");
+		mExpToPdf.setIcon(new ImageIcon("Img/pdf.png"));
 		JMenuItem mExpToMsExcell = new JMenuItem("mExpToMsExcell");
+		mExpToMsExcell.setIcon(new ImageIcon("Img/excell.png"));
 		JMenuItem mExpToMsWord = new JMenuItem("mExpToMsWord");
+		mExpToMsWord.setIcon(new ImageIcon("Img/word.png"));
 		
 		mExport.add(mExpToPdf);
 		mExport.add(mExpToMsExcell);
 		mExport.add(mExpToMsWord);
+		mExport.setIcon(new ImageIcon("Img/export.png"));
 		
 		JMenu mImport = new JMenu("mImport");
 		JMenuItem mImpToPdf = new JMenuItem("mImpToPdf");
+		mImpToPdf.setIcon(new ImageIcon("Img/pdf.png"));
 		JMenuItem mImpToMsExcell = new JMenuItem("mImpToMsExcell");
+		mImpToMsExcell.setIcon(new ImageIcon("Img/excell.png"));
 		JMenuItem mImpToMsWord = new JMenuItem("mImpToMsWord");
+		mImpToMsWord.setIcon(new ImageIcon("Img/word.png"));
 		
 		mImport.add(mImpToPdf);
 		mImport.add(mImpToMsExcell);
 		mImport.add(mImpToMsWord);
+		mImport.setIcon(new ImageIcon("Img/import.png"));
 		
 		JMenuItem mExit = new JMenuItem("mExit");
-		
+		mExit.setIcon(new ImageIcon("Img/exit.png"));
 		
 		file.add(mNew);
 		file.addSeparator();
@@ -60,10 +76,17 @@ public class MenuBar extends JMenuBar{
 		
 		JMenu view = new JMenu("View");
 		JMenu tools = new JMenu("Tools");
+		JMenu parametrizacija = new JMenu("Parametrizacija");
+		JMenu pomoc = new JMenu("Pomoc");
+		JMenu about = new JMenu("About");
 		
 		add(file);
 		add(view);
 		add(tools);
+		add(Box.createHorizontalGlue());
+		add(parametrizacija);
+		add(pomoc);
+		add(about);
 	}
 	
 	
--- InstaFram/trunk/src/instafram/view/ToolBar.java
+++ InstaFram/trunk/src/instafram/view/ToolBar.java
@@ -1,7 +1,51 @@
 package instafram.view;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 public class ToolBar extends JToolBar{
+
+	public ToolBar() {
+		super(SwingConstants.HORIZONTAL);
+		
+		JButton btnNew = new JButton();
+		btnNew.setToolTipText("New");
+		btnNew.setIcon(new ImageIcon("Img/new.png"));
+		add(btnNew);
+		
+		JButton btnOpen = new JButton();
+		btnOpen.setToolTipText("Open");
+		btnOpen.setIcon(new ImageIcon("Img/open.jpg"));
+		add(btnOpen);
+		
+		JButton btnClose = new JButton();
+		btnClose.setToolTipText("Close");
+		btnClose.setIcon(new ImageIcon("Img/close.png"));
+		add(btnClose);
+		
+		JButton btnSwitch = new JButton();
+		btnSwitch.setToolTipText("Switch");
+		btnSwitch.setIcon(new ImageIcon("Img/switch.png"));
+		add(btnSwitch);
+		
+		JButton btnSave = new JButton();
+		btnSave.setToolTipText("Save");
+		btnSave.setIcon(new ImageIcon("Img/save.png"));
+		add(btnSave);
+		
+		JButton btnSaveAs = new JButton();
+		btnSaveAs.setToolTipText("Save");
+		btnSaveAs.setIcon(new ImageIcon("Img/save.png"));
+		add(btnSaveAs);
+		
+		JButton btnExit = new JButton();
+		btnExit.setToolTipText("Exit");
+		btnExit.setIcon(new ImageIcon("Img/exit.png"));
+		add(btnExit);
+		
+		setFloatable(true);
+	}
 
 }
