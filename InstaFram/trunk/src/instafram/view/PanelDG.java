5660be6b1f4e79661fb6a340a6b2cce510db1b16
rn14-2017 <rn14-2017@7147980d-cb72-d84e-875d-273fa6e1a3de>
2018-10-29 22:21:00 +0000

    Dodat Singleton

--- InstaFram/trunk/src/instafram/view/PanelDG.java
+++ InstaFram/trunk/src/instafram/view/PanelDG.java
@@ -6,16 +6,18 @@ import java.awt.Toolkit;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
 
 public class PanelDG extends APanel{
 	
 	public PanelDG() {
 		
+		this.setBorder(new EmptyBorder(0, 0, 0, 0));
 		ToolBar tb = new ToolBar();
-		add(tb, BorderLayout.WEST);
+		add(tb, BorderLayout.NORTH);
 		
 		//privremeno
-		JTextArea ta = new JTextArea("Test za scrollPane", 5, 5);
+		JTextArea ta = new JTextArea("Test za scrollPane", 10, 20);
 		
 		JScrollPane scrollPane = new JScrollPane(ta, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		add(scrollPane, BorderLayout.SOUTH);
