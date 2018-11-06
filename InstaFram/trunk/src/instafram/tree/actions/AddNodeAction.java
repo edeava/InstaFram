38294d1a07be2d2f2af5ff853de2baaa8246c0a5
rn14-2017 <rn14-2017@7147980d-cb72-d84e-875d-273fa6e1a3de>
2018-11-06 13:30:44 +0000

    Korigovana klasa ZTreeAbsAction i akcije koje je nasledjuju.

--- InstaFram/trunk/src/instafram/tree/actions/AddNodeAction.java
+++ InstaFram/trunk/src/instafram/tree/actions/AddNodeAction.java
@@ -11,7 +11,7 @@ import instafram.tree.model.ZTreeNode;
 import instafram.treeComponent.model.Kompanija;
 import instafram.view.Application;
 
-public class AddNodeAction extends ZTreeAbsAction implements Observer{
+public class AddNodeAction extends ZTreeAbsAction{
 
 	public static int i = 1;
 	
@@ -24,16 +24,5 @@ public class AddNodeAction extends ZTreeAbsAction implements Observer{
 		if(selectedNode != null)
 			controller.addNode(selectedNode, new Kompanija("Kompanija" + AddNodeAction.i++));
 		else JOptionPane.showMessageDialog(Application.getInstance(), "Nevalidno dodavanje");
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setSelectedNode(ZTreeNode selectedNode) {
-		this.selectedNode = selectedNode;
-		this.setEnabled(this.selectedNode.getAllowsChildren());
 	}
 }
--- InstaFram/trunk/src/instafram/tree/actions/LoadTreeAction.java
+++ InstaFram/trunk/src/instafram/tree/actions/LoadTreeAction.java
@@ -10,7 +10,7 @@ import javax.swing.JFileChooser;
 import instafram.tree.controller.IZTreeController;
 import instafram.tree.model.ZTreeNode;
 
-public class LoadTreeAction extends ZTreeAbsAction implements Observer{
+public class LoadTreeAction extends ZTreeAbsAction{
 
 	public LoadTreeAction(IZTreeController controller) {
 		super(controller);
@@ -28,19 +28,6 @@ public class LoadTreeAction extends ZTreeAbsAction implements Observer{
 			if(selectedNode != null)
 				controller.loadTree(selectedNode, file);
 		}
-	}
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setSelectedNode(ZTreeNode selectedNode) {
-		this.selectedNode = selectedNode;
-		this.setEnabled(this.selectedNode.getAllowsChildren());
-		
 	}
 
 }
