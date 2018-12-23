70085a9003bc20bd833e8759108dea1b35293463
rn14-2017 <33694893+edeava@users.noreply.github.com>
2018-12-23 01:12:15 +0000

    Popravljeno selektovanje nodova za Cut i Copy.

--- src/instafram/tree/actions/CopyNodeAction.java
+++ src/instafram/tree/actions/CopyNodeAction.java
@@ -4,6 +4,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
+import javax.swing.tree.TreePath;
+
 import instafram.tree.controller.IZTreeController;
 import instafram.tree.model.TreeElementSelection;
 import instafram.tree.model.ZTreeNode;
@@ -21,6 +23,12 @@ public class CopyNodeAction extends ZTreeAbsAction{
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
+		selectedNodes.clear();
+		TreePath[] paths = controller.getTree().getSelectionPaths();
+		int n = paths.length;
+		for (int i = 0; i < n; i++) 
+			selectedNodes.add((ZTreeNode) paths[i].getPathComponent(paths[i].getPathCount() - 1));
+		
 		TreeElementSelection selection = new TreeElementSelection(selectedNodes);
 		clipboard.setContents(selection, Application.getInstance());
 	}
